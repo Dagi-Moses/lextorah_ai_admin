cf82ad99968b7efa4d14af5aa984cbed5be7b4ad
Tahavmbuter <dagitmoses@gmail.com>
2025-05-20 10:54:00 +0100

    nnn

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -52,42 +52,42 @@ class AppRouter {
       debugLogDiagnostics: true,
       initialLocation: AppRoutePath.login,
 
-      // redirect: (context, state) {
-      //   final isAuth = authState.isAuthenticated;
-      //   final UserRole? role = authState.user?.role;
-      //   final location = state.uri.toString();
+      redirect: (context, state) {
+        final isAuth = authState.isAuthenticated;
+        final UserRole? role = authState.user?.role;
+        final location = state.uri.toString();
 
-      //   // 🚫 If user is not authenticated and trying to access protected path
-      //   if (!isAuth && !publicPaths.any((path) => location.startsWith(path))) {
-      //     return AppRoutePath.login;
-      //   }
-      //   // If authenticated but at login, redirect to role dashboard
-      //   if (isAuth && location == AppRoutePath.login) {
-      //     if (role?.isAdmin ?? false) {
-      //       return AppRoutePath.upload;
-      //     } else {
-      //       return AppRoutePath.chat;
-      //     }
-      //   }
+        // 🚫 If user is not authenticated and trying to access protected path
+        if (!isAuth && !publicPaths.any((path) => location.startsWith(path))) {
+          return AppRoutePath.login;
+        }
+        // If authenticated but at login, redirect to role dashboard
+        if (isAuth && location == AppRoutePath.login) {
+          if (role?.isAdmin ?? false) {
+            return AppRoutePath.upload;
+          } else {
+            return AppRoutePath.chat;
+          }
+        }
 
-      //   // Prevent student from accessing admin routes and vice versa
-      //   if (isAuth &&
-      //       (role?.isStudent ?? false) &&
-      //       location == AppRoutePath.upload) {
-      //     return AppRoutePath.chat;
-      //   }
+        // Prevent student from accessing admin routes and vice versa
+        if (isAuth &&
+            (role?.isStudent ?? false) &&
+            location == AppRoutePath.upload) {
+          return AppRoutePath.chat;
+        }
 
-      //   // if (isAuth &&
-      //   //     (role?.isAdmin ?? false) &&
-      //   //     location == AppRoutePath.chatbot) {
-      //   //   return AppRoutePath.upload;
-      //   // }
+        // if (isAuth &&
+        //     (role?.isAdmin ?? false) &&
+        //     location == AppRoutePath.chatbot) {
+        //   return AppRoutePath.upload;
+        // }
 
-      //   return null; // No redirection
-      // },
-      // refreshListenable: GoRouterRefreshStream(
-      //   ref.watch(authProvider.notifier).authChanges,
-      // ), // Add stream to listen to auth changes
+        return null; // No redirection
+      },
+      refreshListenable: GoRouterRefreshStream(
+        ref.watch(authProvider.notifier).authChanges,
+      ), // Add stream to listen to auth changes
       errorBuilder:
           (context, state) =>
               ErrorScreen(message: state.error?.toString() ?? "Page not found"),
